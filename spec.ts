d26f120d3ed2a1b15cea670e870052d7166d6d4e
djtinkers <djtinkers365@gmail.com>
2020-02-04 14:25:32 -0500

    nothing special

---
+++ spec.ts
@@ -0,0 +1,13 @@
+console.warn("REFER to 'Codebrarian-namespace' for naming conventions");
+console.warn(`
+    * boot
+    * cold boot
+    * hot  boot
+`);
+console.warn(`
+    * before boot
+    * after boot
+`);
+console.warn(`
+    * what interface do I mount objects to?
+`);
